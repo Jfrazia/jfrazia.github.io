1acb13293143bfb42f9b07ed53ae5ac32b69f2bd
John Doe <43723279+Brian-Earl@users.noreply.github.com>
2022-01-07 12:59:13 -0500

    Allow for no buttons
    Add feature in which the adding of the buttons will be skipped if the "links" section of the json are missing

--- index.js
+++ index.js
@@ -2,107 +2,90 @@
 
 const body = document.getElementById("cardBody");
 
-$.getJSON('https://raw.githubusercontent.com/Jfrazia/jfrazia.github.io/main/index.json', function (data) {
+$.getJSON(
+  "https://raw.githubusercontent.com/Jfrazia/jfrazia.github.io/main/index.json",
+  function (data) {
     data.cards.forEach(createCard);
-});
+  }
+);
 
 function createCard(cardData) {
-    base = document.createElement('div');
-    base.className = "column is-one-third-desktop is-full-touch"
-    body.appendChild(base)
+  base = document.createElement("div");
+  base.className = "column is-one-third-desktop is-full-touch";
+  body.appendChild(base);
 
-    frame = document.createElement('div');
-    frame.className = "box has-background-grey-lighter"
-    base.appendChild(frame)
+  frame = document.createElement("div");
+  frame.className = "box has-background-grey-lighter";
+  base.appendChild(frame);
 
-    card = document.createElement('div')
-    card.className = "card equal-height"
-    frame.appendChild(card)
+  card = document.createElement("div");
+  card.className = "card equal-height";
+  frame.appendChild(card);
 
-    cardImage = document.createElement('div')
-    cardImage.className = "card-image"
-    card.appendChild(cardImage)
+  cardImage = document.createElement("div");
+  cardImage.className = "card-image";
+  card.appendChild(cardImage);
 
-    cardFigure = document.createElement('figure')
-    cardFigure.className = "image is-16by9"
-    cardImage.appendChild(cardFigure)
+  cardFigure = document.createElement("figure");
+  cardFigure.className = "image is-16by9";
+  cardImage.appendChild(cardFigure);
 
-    cardImageSrc = document.createElement('img')
-    cardImageSrc.src = cardData.image
-    cardImageSrc.alt = "Loading..."
-    cardFigure.appendChild(cardImageSrc)
+  cardImageSrc = document.createElement("img");
+  cardImageSrc.src = cardData.image;
+  cardImageSrc.alt = "Loading...";
+  cardFigure.appendChild(cardImageSrc);
 
-    cardContent = document.createElement('div')
-    cardContent.className = "card-content"
-    card.appendChild(cardContent)
+  cardContent = document.createElement("div");
+  cardContent.className = "card-content";
+  card.appendChild(cardContent);
 
-    cardTitle = document.createElement('div')
-    cardTitle.className = "Media"
-    cardContent.appendChild(cardTitle)
+  cardTitle = document.createElement("div");
+  cardTitle.className = "Media";
+  cardContent.appendChild(cardTitle);
 
-    cardMediaContent = document.createElement('div')
-    cardMediaContent.className = "media-content"
-    cardTitle.appendChild(cardMediaContent)
+  cardMediaContent = document.createElement("div");
+  cardMediaContent.className = "media-content";
+  cardTitle.appendChild(cardMediaContent);
 
-    cardTitleText = document.createElement('p')
-    cardTitleText.className = "title is-4"
-    cardTitleText.innerHTML = cardData.title
-    cardMediaContent.appendChild(cardTitleText)
+  cardTitleText = document.createElement("p");
+  cardTitleText.className = "title is-4";
+  cardTitleText.innerHTML = cardData.title;
+  cardMediaContent.appendChild(cardTitleText);
 
-    cardTags = document.createElement('div')
-    cardTags.className = "tags"
-    cardContent.appendChild(cardTags)
+  cardTags = document.createElement("div");
+  cardTags.className = "tags";
+  cardContent.appendChild(cardTags);
 
+  if (cardData.tags != undefined || cardData.tags.length == 0) {
     cardData.tags.forEach((element) => {
-        tagSpan = document.createElement('span')
-        tagSpan.className = "tag " + element.color
-        tagSpan.innerHTML = element.title
-        cardTags.appendChild(tagSpan)
+      tagSpan = document.createElement("span");
+      tagSpan.className = "tag " + element.color;
+      tagSpan.innerHTML = element.title;
+      cardTags.appendChild(tagSpan);
     });
+  }
+  
+  cardMainBody = document.createElement("div");
+  cardMainBody.className = "card-main-body";
+  cardMainBody.innerHTML = cardData.description;
+  cardContent.appendChild(cardMainBody);
 
-    cardMainBody = document.createElement('div')
-    cardMainBody.className = "card-main-body"
-    cardMainBody.innerHTML = cardData.description
-    //truncate(cardMainBody, 200, '...')
-    cardContent.appendChild(cardMainBody)
-
-    cardFooter = document.createElement('footer')
-    cardFooter.className = "card-footer"
-    card.appendChild(cardFooter)
+  if (cardData.links != undefined || cardData.links.length == 0) {
+    cardFooter = document.createElement("footer");
+    cardFooter.className = "card-footer";
+    card.appendChild(cardFooter);
 
     cardData.links.forEach((element) => {
-        buttonLink = document.createElement('a')
-        buttonLink.href = element.link
+      if (element.link != undefined && element.title != undefined) {
+        buttonLink = document.createElement("a");
+        buttonLink.href = element.link;
         //buttonLink.style = "margin-right:10px;"
-        cardFooter.appendChild(buttonLink)
-        button = document.createElement('button')
-        button.className = "card-footer-item button is-link"
-        button.innerHTML = element.title
-        buttonLink.appendChild(button)
+        cardFooter.appendChild(buttonLink);
+        button = document.createElement("button");
+        button.className = "card-footer-item button is-link";
+        button.innerHTML = element.title;
+        buttonLink.appendChild(button);
+      }
     });
-}
-
-var truncate = function (elem, limit, after) {
-    if (elem.innerHTML.length > limit) {
-        //(content.count('<br>')
-        var content = elem.innerHTML
-        content = elem.innerHTML.slice(0, limit)
-        console.log(content)
-        // if ((content.count('<br><br>')) > 0) {
-        //     content = content.slice(0, length - (content.count('<br><br>') * 50))
-        // }
-        content = reverse(content)
-        content = content.slice(content.indexOf(' ') + 1, content.length)
-        content = reverse(content)
-        content += after
-        elem.innerHTML = content
-    }
-};
-
-function reverse(s) {
-    return s.split("").reverse().join("");
-}
-
-String.prototype.count = function (s1) {
-    return (this.length - this.replace(new RegExp(s1, "g"), '').length) / s1.length;
+  }
 }
